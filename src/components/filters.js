ce1c815d9921070b856f7312e76c90024a5cca1a
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2018-03-13 09:35:03 +0100

    insert default checked prop to filter radio buttons

--- src/components/filters.js
+++ src/components/filters.js
@@ -9,6 +9,7 @@ const Filters = ({ filter, updateFilter }) => (
           type="radio"
           name="radio"
           id="radio-1"
+          defaultChecked={Boolean(filter === FILTERS.ALL)}
           onClick={() => updateFilter(FILTERS.ALL)}
         />
         <label className="label" htmlFor="radio-1">
@@ -20,6 +21,7 @@ const Filters = ({ filter, updateFilter }) => (
           type="radio"
           name="radio"
           id="radio-2"
+          defaultChecked={Boolean(filter === FILTERS.DONE)}
           onClick={() => updateFilter(FILTERS.DONE)}
         />
         <label className="label" htmlFor="radio-2">
@@ -31,6 +33,7 @@ const Filters = ({ filter, updateFilter }) => (
           type="radio"
           name="radio"
           id="radio-3"
+          defaultChecked={Boolean(filter === FILTERS.PENDING)}
           onClick={() => updateFilter(FILTERS.PENDING)}
         />
         <label className="label" htmlFor="radio-3">
